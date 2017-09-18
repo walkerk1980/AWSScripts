c3c2c66e2ce6e16a10f0b3e41f2d30e4b928b54c
Keith Walker <walkerk1980@users.noreply.github.com>
2017-09-18 13:19:49 -0700

    Create pullParseCloudTrail.sh

---
+++ pullParseCloudTrail.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+akid=0123456789012
+region='us-west-2'
+bucket='cloudtrail-bucket-name'
+#prefix if your Trail has one
+prefix='CloudTrail'
+account=0123456789012
+
+year=$(date +%Y)
+month=$(date +%m)
+day=$(date +%d)
+mkdir ctlogs
+cd ctlogs/
+
+#pull from s3
+aws s3 cp s3://$bucket/AWSLogs/$account/CloudTrail/$region/$year/$month/$day/ ./ --recursive
+aws s3 cp s3://$bucket/$prefix/AWSLogs/$account/CloudTrail/$region/$year/$month/$day/ ./ --recursive
+
+#without jq package installed
+zgrep -r $akid *|cut -d ':' -f2-999
+
+#with jq installed
+zgrep -r $akid *|cut -d ':' -f2-999|jq -r
