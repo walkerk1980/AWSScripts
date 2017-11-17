721c17acf2f38e0e4f9e099898af0703b2c76a45
Keith Walker <walkerk1980@users.noreply.github.com>
2017-11-16 18:23:06 -0800

    Update CBCPaddingExample.sh

--- CBCPaddingExample.sh
+++ CBCPaddingExample.sh
@@ -19,7 +19,7 @@ echo -e "\n\rDecrypting ciphertext.bin with AES-128-CBC using our Key and the wr
 openssl enc -d -aes-128-cbc -in ciphertext.bin -K `cat key.hex` -iv `openssl rand -hex 16`
 
 echo -e "\n\rAs you can see, when the wrong IV is supplied to CBC decrytion you will lose the first block of data,\n\r\
-after the first block of data you are fine because you have the previous block's ciphertext. This is not the case with\n\r\\
-the GCM algorithm because you XOR after decryption with the previous block's plaintext instead of it's ciphertext, which\n\r\\
-is not known to you because you cannot decrypt the previous block's plaintext for any block if you do not start with the\n\r\\
+after the first block of data you are fine because you have the previous block's ciphertext. This is not the case with\n\r\
+the GCM algorithm because you XOR after decryption with the previous block's plaintext instead of it's ciphertext, which\n\r\
+is not known to you because you cannot decrypt the previous block's plaintext for any block if you do not start with the\n\r\
 correct IV.\n\r"
