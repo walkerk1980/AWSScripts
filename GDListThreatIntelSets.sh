483971a258b8540c3da86049aba2d39e54fad487
Keith Walker <walkerk1980@users.noreply.github.com>
2018-02-21 15:38:06 -0800

    Update GDListThreatIntelSets.sh

--- GDListThreatIntelSets.sh
+++ GDListThreatIntelSets.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+region='us-west-2';
+
 detector=$(aws guardduty list-detectors --query DetectorIds --output text --region $region) && for setid in $(aws guardduty list-threat-intel-sets --detector-id $detector --output text --query ThreatIntelSetIds --region $region); do aws guardduty get-threat-intel-set --threat-intel-set-id $setid --detector-id $detector --region $region; done
 
 for list in $(for setid in $(aws guardduty list-threat-intel-sets --detector-id $detector --output text --query ThreatIntelSetIds --region $region); do aws guardduty get-threat-intel-set --threat-intel-set-id $setid --detector-id $detector --query Location --output text --region $region; done); do aws s3 cp $list -; done
