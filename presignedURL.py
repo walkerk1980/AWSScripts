8997e9bb802c848cec8e470d052d7719fa9891f6
Keith Walker <walkerk1980@users.noreply.github.com>
2018-08-16 17:50:49 -0700

    Update presignedURL.py

--- presignedURL.py
+++ presignedURL.py
@@ -1,51 +1,30 @@
 #!/usr/bin/python3
+
+# This script is provided 'AS IS' - use is at own risk!
+# Importing base modules
 import boto3
-#http://docs.python-requests.org/en/master/
 import requests
-import sys
 from botocore.client import Config
 
-region = input('Please enter your bucket\'s region (e.g. \'us-east-1\'):')
+# Getting the bucket region and setting client:
+region = input('Please enter the region your bucket is hosted in (e.g. \'us-west-1\'):')
 
-#instantiate client
 s3 = boto3.client('s3',region_name=region, config=Config(s3={'addressing_style': 'path'}))
 
-# Bucket Name
-upload_bucket = input('Enter the name of the bucket to upload file to: ')
+# Getting bucket name:
+customer_bucket = input('Enter the name of the bucket to upload logs to(e.g. \'my-bucket\'): ')
 
-#Destination object name
-key_name = input('What would you like the file to be name in your bucket: ')
+# Name of the object that will be uploaded to S3 - something logical is suggested,
+key_name = input('Enter the object name of the file to be upload(e.g. \'file_name.tar\'): ')
 
-#local file to upload
-fileName = input('What is the full path of the local file you want to upload: ')
-
-#http://boto3.readthedocs.io/en/latest/reference/services/s3.html#S3.Client.generate_presigned_post
-#param for request, file handle, read binary
-try:
-  file = {'file': open(fileName, 'rb')}
-except OSError as err:
-  print("Local File Error: {0}".format(err))
-  exit()
-
-#generate presigned post for request
-post = s3.generate_presigned_post(
+url = s3.generate_presigned_url(
+    ClientMethod='put_object',
+    Params={
+        'Bucket': customer_bucket,
+        'Key': key_name
+    },
     ExpiresIn=172800,
-    Bucket=upload_bucket,
-    Key=key_name
+    HttpMethod='PUT'
 )
 
-#http://boto3.readthedocs.io/en/latest/reference/services/s3.html#S3.Client.generate_presigned_url
-#upload file post request
-requests.post(post['url'], data=post['fields'], files=file)
-
-#generate presgined download url
-get = s3.generate_presigned_url(
-    'get_object',
-    Params={'Bucket': upload_bucket,'Key': key_name},
-    ExpiresIn=172800,
-)
-
-#print presigned url
-print()
-print("This is the presigned URL to download the s3 object you just uploaded: ")
-print(get)
+print('Please copy the below and share it with the person you wish to receive the file from:\n\r\n\r' + 'curl --request PUT --upload-file /path/to/file.tar ' + '"' + str(url) + '"' + '\n\r')
