6e8e22fbac000267a332440e7a36e1ad2d6fbf91
Keith Walker <walkerk1980@gmail.com>
2019-02-02 00:33:37 +0000

    lastest developing code for ACM DNS check

--- ACMDNSValidationCheck.py
+++ ACMDNSValidationCheck.py
@@ -57,6 +57,97 @@ def continue_yn(message):
   else:
     return(0)
 
+def get_record(name, type):
+  return dns_resolver.query(name, type)
+
+def list_all_public_hosted_zones():
+  print('\n\rPublic Hosted Zones: ')
+  hosted_zones = r53.list_hosted_zones()
+  for hz in hosted_zones['HostedZones']:
+    if hz['Config']['PrivateZone'] == False:
+      print('\n\rHosted Zone Id: ' + str(hz['Id'].split('/')[2]) )
+      print('Hosted Zone Name: ' + str(hz['Name']) )
+
+def get_hz_delegation(hz_id):
+  try:
+    hz_delegation = []
+    get_hz_response = r53.get_hosted_zone(Id=hz_id)
+    print('Hosted Zone Delegation Set: ')
+    for ns in get_hz_response['DelegationSet']['NameServers']:
+      if not str(ns).endswith('.'):
+        ns = str(ns) + '.'
+      hz_delegation.extend([str(ns)])
+    print(hz_delegation)
+    return(hz_delegation)
+  except:
+    print('Error pulling hosted zone delegation')
+    return(hz_delegation)
+
+def get_domain_ns(domain):
+  try:
+    domain_ns = []
+    domain_ns_records=get_record(domain, 'NS')
+    print('Domain NS records pulled from DNS: ')
+    for ns_rr in domain_ns_records:
+      if not str(ns_rr).endswith('.'):
+        ns_rr = str(ns_rr) + '.'
+      domain_ns.extend([str(ns_rr)])
+    print(domain_ns)
+    return(domain_ns)
+  except:
+    print('Could not pull NS records for ' + domain)
+    return([])
+
+def get_domain_rr(domain, type):
+  try:
+    domain_rr = []
+    domain_rr_records=get_record(domain, type)
+    print('Domain ' + str(type) + ' records pulled from DNS: ')
+    for rr in domain_rr_records:
+      if type == 'NS'  and not str(rr).endswith('.'):
+        rr = str(ns_rr) + '.'
+      domain_ns.extend([str(rr)])
+    print(domain_rr)
+    return(domain_rr)
+  except:
+    print('Could not pull ' + type + ' records for ' + domain)
+    return([])
+
+
+def get_hosted_zone_by_domain(domain):
+  print('\n\rSearching for HZ for ' + domain + '\n\r')
+  hosted_zones = r53.list_hosted_zones()
+  if len(hosted_zones['HostedZones']) <1:
+    print('No Hosted Zones found in Account\n\r')
+  for hz in hosted_zones['HostedZones']:
+    hz_delegation = []
+    domain_ns = []
+    if hz['Config']['PrivateZone'] == False:
+      if str(hz['Name']) == str(domain) or str(hz['Name']) == str(domain) + '.':
+        print('Found Hosted Zone with matching domain..')
+        hosted_zone_id = str(hz['Id'].split('/')[2])
+        print('\n\rHosted Zone Id: ' + hosted_zone_id )
+        print('Hosted Zone Name: ' + str(hz['Name']) )
+        domain_ns = get_domain_ns(domain)
+#        domain_ns = get_domain_rr(domain, type='NS')
+        hz_delegation = get_hz_delegation(hosted_zone_id)
+        domain_ns.sort()
+        hz_delegation.sort()
+        if domain_ns == hz_delegation:
+          print('\n\rThis Hosted Zone\'s Delegation and NS records for domain match!')
+          print(hz_delegation)
+          print(domain_ns)
+          create_rr=continue_yn('\n\rI can add a CNAME record for this certificate in this hosted zone: ' + hosted_zone_id)
+          if create_rr == 1:
+            create_record_set()
+        else:
+          print('This Hosted Zone\'s Delegation does not match NS records for domain.')
+
+
+def create_record_set():
+  print('Creating DNS Record')
+  #TODO
+
 def list_pending_certs():
   print('\n\rCertificates pending DNS Validation: ')
   certs = acm.list_certificates()
@@ -90,77 +181,6 @@ def list_pending_certs():
             cert_domain=cert_full_domain[cert_full_domain.index('.')+1:]
             get_hosted_zone_by_domain(cert_domain)
 
-def get_record(name, type):
-  return dns_resolver.query(name, type)
-
-def list_all_public_hosted_zones():
-  print('\n\rPublic Hosted Zones: ')
-  hosted_zones = r53.list_hosted_zones()
-  for hz in hosted_zones['HostedZones']:
-    if hz['Config']['PrivateZone'] == False:
-      print('\n\rHosted Zone Id: ' + str(hz['Id'].split('/')[2]) )
-      print('Hosted Zone Name: ' + str(hz['Name']) )
-
-def get_hosted_zone_by_domain(domain):
-  print('\n\rSearching for HZ for ' + domain + '\n\r')
-  hosted_zones = r53.list_hosted_zones()
-  if len(hosted_zones['HostedZones']) <1:
-    print('No Hosted Zones found in Account\n\r')
-  for hz in hosted_zones['HostedZones']:
-    hz_delegation = []
-    domain_ns = []
-    if hz['Config']['PrivateZone'] == False:
-      if str(hz['Name']) == str(domain) or str(hz['Name']) == str(domain) + '.':
-        print('Found Hosted Zone with matching domain..')
-        hosted_zone_id = str(hz['Id'].split('/')[2])
-        print('\n\rHosted Zone Id: ' + hosted_zone_id )
-        print('Hosted Zone Name: ' + str(hz['Name']) )
-        domain_ns = get_domain_ns(domain)
-        hz_delegation = get_hz_delegation(hosted_zone_id)
-        domain_ns.sort()
-        hz_delegation.sort()
-        if domain_ns == hz_delegation:
-          print('\n\rThis Hosted Zone\'s Delegation and NS records for domain match!')
-          print(hz_delegation)
-          print(domain_ns)
-          create_rr=continue_yn('\n\rI can add a CNAME record for this certificate in this hosted zone: ' + hosted_zone_id)
-          if create_rr == 1:
-            create_record_set()
-        else:
-          print('This Hosted Zone\'s Delegation does not match NS records for domain.')
-
-def get_hz_delegation(hz_id):
-  try:
-    hz_delegation = []
-    get_hz_response = r53.get_hosted_zone(Id=hz_id)
-    print('Hosted Zone Delegation Set: ')
-    for ns in get_hz_response['DelegationSet']['NameServers']:
-      if not str(ns).endswith('.'):
-        ns = str(ns) + '.'
-      hz_delegation.extend([str(ns)])
-    print(hz_delegation)
-    return(hz_delegation)
-  except:
-    print('Error pulling hosted zone delegation')
-    return(hz_delegation)
-
-def get_domain_ns(domain):
-  try:
-    domain_ns = []
-    domain_ns_records=get_record(domain, 'NS')
-    print('Domain NS records pulled from DNS: ')
-    for ns_rr in domain_ns_records:
-      if not str(ns_rr).endswith('.'):
-        ns_rr = str(ns_rr) + '.'
-      domain_ns.extend([str(ns_rr)])
-    print(domain_ns)
-    return(domain_ns)
-  except:
-    print('Could not pull NS records for ' + domain)
-    return([])
-
-def create_record_set():
-  print('Creating DNS Record')
 
 
 list_pending_certs()
